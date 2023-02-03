48433d1c7b3141a2bef85b9058b20aa28c98231e
vlasJedi <azmodei@protonmail.com>
2023-02-04 00:24:05 +0200

    added-ol-map

--- frontend/angular-app/src/app/root/components/home-page/home-page.component.ts
+++ frontend/angular-app/src/app/root/components/home-page/home-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewEncapsulation} from '@angular/core';
+
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class HomePageComponent {
+export class HomePageComponent implements AfterViewInit {
+  private map?: Map;
 
+  ngAfterViewInit() {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+  }
 }
